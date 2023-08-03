70afb90c1cc01880375102dfa811062e64e55807
Nicholas <infinitevolume@gmail.com>
2023-08-03 13:32:02 -0700

    Allowed cube to animate

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-const three = THREE;
-// Scene Mesh Camera Renderer
+//Scene Mesh Camera Renderer
+const scene = new THREE.Scene();
 
-//Scene
-const scene = new three.Scene();
-
-// Group
-const group = new three.Group();
-
-//Mesh
-const geometry = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({ color: 'purple' });
-
-const mesh = new three.Mesh(geometry, material);
-mesh.position.z = 1;
-
-mesh.scale.x = 2;
-
-// MeshTWo
-const geometryT = new three.BoxGeometry(1, 1, 1);
-const materialT = new three.MeshBasicMaterial({ color: 'green' });
-const meshT = new three.Mesh(geometryT, materialT);
-meshT.position.y = 2;
-
-group.add(mesh, meshT);
-group.position.x = 2;
-scene.add(group);
-
-// AxesHelper
-const axesHelper = new three.AxesHelper(4);
-scene.add(axesHelper);
+//Mesh / Objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'purple' });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 //Camera
 const aspect = {
@@ -36,14 +13,23 @@ const aspect = {
   height: window.innerHeight,
 };
 
-const camera = new three.PerspectiveCamera(75, aspect.width / aspect.height); //near value is 1, and far value is 2000
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
 camera.position.z = 5;
-camera.position.x = 1;
-camera.position.y = 1;
 scene.add(camera);
 
-//Renderer
-const canvas = document.querySelector('.draw'); //select the canvas element
-const renderer = new three.WebGLRenderer({ canvas }); //add the WebGLRenderer
+// Renderer
+const canvas = document.querySelector('.draw');
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(aspect.width, aspect.height); //Renderer size
-renderer.render(scene, camera); //display what the camera in the scene captured
+
+// Clock Class
+const clock = new THREE.Clock();
+
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+
+  cube.rotation.y = elapsedTime;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+animate();
