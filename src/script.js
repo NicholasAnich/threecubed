cf5faf932ee1379a1af90ea2fb82168ee6a7ff14
Nicholas <infinitevolume@gmail.com>
2023-08-09 15:11:20 -0700

    added rectAreaLight to scene

--- src/script.js
+++ src/script.js
@@ -17,7 +17,7 @@ const gui = new dat.GUI();
 // pointLight.position.set(0, 2, 2);
 // scene.add(ambientLight, pointLight);
 
-const ambientLight = new THREE.AmbientLight('#ffffff', 1);
+// const ambientLight = new THREE.AmbientLight('#ffffff', 1);
 // scene.add(ambientLight);
 // gui
 //   .add(ambientLight, 'intensity')
@@ -26,9 +26,9 @@ const ambientLight = new THREE.AmbientLight('#ffffff', 1);
 //   .step(0.1)
 //   .name('Intensity One');
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 // scene.add(directionalLight);
-directionalLight.position.set(0, 2, 0);
+// directionalLight.position.set(0, 2, 0);
 // gui
 //   .add(directionalLight, 'intensity')
 //   .min(0)
@@ -39,32 +39,39 @@ directionalLight.position.set(0, 2, 0);
 // gui.add(directionalLight.position, 'y').min(-3).max(3).step(0.01).name('Y Dir');
 
 // DirectionalLightHelper;
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight
-);
+// const directionalLightHelper = new THREE.DirectionalLightHelper(
+//   directionalLight
+// );
 // scene.add(directionalLightHelper);
 
 //HemisphereLight
-const hemisphereLight = new THREE.HemisphereLight('magenta', 'blue', 0);
+// const hemisphereLight = new THREE.HemisphereLight('magenta', 'blue', 0);
 // scene.add(hemisphereLight);
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight);
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight);
 // scene.add(hemisphereLightHelper);
 
-// Point Light
-const pointLight = new THREE.PointLight('red', 1, 50);
-scene.add(pointLight);
-pointLight.position.set(0, 2, 0);
-const pointLightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(pointLightHelper);
-gui.add(pointLight.position, 'x').min(-3).max(3).step(0.1).name('X Point');
-gui.add(pointLight.position, 'y').min(-3).max(3).step(0.1).name('Y Point');
-gui.add(pointLight.position, 'z').min(-3).max(3).step(0.1).name('Z Point');
-gui
-  .add(pointLight, 'intensity')
-  .min(-5)
-  .max(5)
-  .step(0.1)
-  .name('Point Intensity');
+//4 ) Point Light
+// const pointLight = new THREE.PointLight('red', 1, 50);
+// scene.add(pointLight);
+// pointLight.position.set(0, 2, 0);
+// const pointLightHelper = new THREE.PointLightHelper(pointLight);
+// scene.add(pointLightHelper);
+// gui.add(pointLight.position, 'x').min(-3).max(3).step(0.1).name('X Point');
+// gui.add(pointLight.position, 'y').min(-3).max(3).step(0.1).name('Y Point');
+// gui.add(pointLight.position, 'z').min(-3).max(3).step(0.1).name('Z Point');
+// gui
+//   .add(pointLight, 'intensity')
+//   .min(-5)
+//   .max(5)
+//   .step(0.1)
+//   .name('Point Intensity');
+
+//5 ) Direct Area Light
+const rectAreaLight = new THREE.RectAreaLight('#5D3FD3', 3, 2, 2);
+rectAreaLight.position.z = 0.5;
+scene.add(rectAreaLight);
+gui.add(rectAreaLight, 'width').min(1).max(5).step(0.1).name('width');
+gui.add(rectAreaLight, 'height').min(1).max(5).step(0.1).name('height');
 
 //LoadingManger
 const loadingManager = new THREE.LoadingManager();
@@ -81,69 +88,69 @@ loadingManager.onError = () => {
   console.log('Error!');
 };
 //texture Loader
-const textureLoader = new THREE.TextureLoader(loadingManager);
+// const textureLoader = new THREE.TextureLoader(loadingManager);
 
-const colorTexture = textureLoader.load('/texture/color.jpg');
-const otherTextureLoader = new THREE.TextureLoader(loadingManager);
-const otherColorTexture = otherTextureLoader.load('/texture/Normal.jpg');
-const matcaptTexture = textureLoader.load('/texture/mat2.png');
-const bumpTexture = textureLoader.load('/texture/bump.jpg');
-const displacementTexture = textureLoader.load('/texture/displacementMap.jpg');
+// const colorTexture = textureLoader.load('/texture/color.jpg');
+// const otherTextureLoader = new THREE.TextureLoader(loadingManager);
+// const otherColorTexture = otherTextureLoader.load('/texture/Normal.jpg');
+// const matcaptTexture = textureLoader.load('/texture/mat2.png');
+// const bumpTexture = textureLoader.load('/texture/bump.jpg');
+// const displacementTexture = textureLoader.load('/texture/displacementMap.jpg');
 
 //Cube texture Loader
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envTexture = cubeTextureLoader.load([
-  '/texture/env/px.png',
-  '/texture/env/nx.png',
-  '/texture/env/py.png',
-  '/texture/env/ny.png',
-  '/texture/env/pz.png',
-  '/texture/env/nz.png',
-]);
+// const cubeTextureLoader = new THREE.CubeTextureLoader();
+// const envTexture = cubeTextureLoader.load([
+//   '/texture/env/px.png',
+//   '/texture/env/nx.png',
+//   '/texture/env/py.png',
+//   '/texture/env/ny.png',
+//   '/texture/env/pz.png',
+//   '/texture/env/nz.png',
+// ]);
 
 // scene.background = envTexture;
 
 //Mesh / Objects
 // CUBE
-const geometry1 = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5, 2, 2, 2);
-const material1 = new THREE.MeshBasicMaterial({
-  color: 'green',
-  wireframe: true,
-});
-const cubeGreen = new THREE.Mesh(geometry1, material1);
-cubeGreen.position.set(0, 0, 1);
+// const geometry1 = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5, 2, 2, 2);
+// const material1 = new THREE.MeshBasicMaterial({
+//   color: 'green',
+//   wireframe: true,
+// });
+// const cubeGreen = new THREE.Mesh(geometry1, material1);
+// cubeGreen.position.set(0, 0, 1);
 
 // CUBE PURPLE
-const materialColor = {
-  color: 0xffffff,
-};
-const geometry2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material2 = new THREE.MeshStandardMaterial({ color: 'purple' });
-const cubePurple = new THREE.Mesh(geometry2, material2);
-cubePurple.position.set(1, 0, 1);
+// const materialColor = {
+//   color: 0xffffff,
+// };
+// const geometry2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+// const material2 = new THREE.MeshStandardMaterial({ color: 'purple' });
+// const cubePurple = new THREE.Mesh(geometry2, material2);
+// cubePurple.position.set(1, 0, 1);
 
 // 1) RANGE GUI
-gui.add(cubePurple.position, 'x').min(-3).max(3).step(0.1).name('x cubePurple');
+// gui.add(cubePurple.position, 'x').min(-3).max(3).step(0.1).name('x cubePurple');
 // 2) BOOLEAN
-gui.add(material2, 'wireframe');
+// gui.add(material2, 'wireframe');
 // 3) COLOR
-gui.addColor(materialColor, 'color').onChange(() => {
-  material2.color.set(materialColor.color);
-});
+// gui.addColor(materialColor, 'color').onChange(() => {
+//   material2.color.set(materialColor.color);
+// });
 
-const geometry3 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material3 = new THREE.MeshBasicMaterial({ color: 'yellow' });
-material3.color = new THREE.Color(0xf36886);
-const cubeYellow = new THREE.Mesh(geometry3, material3);
-cubeYellow.position.set(-1, 0, 1);
+// const geometry3 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+// const material3 = new THREE.MeshBasicMaterial({ color: 'yellow' });
+// material3.color = new THREE.Color(0xf36886);
+// const cubeYellow = new THREE.Mesh(geometry3, material3);
+// cubeYellow.position.set(-1, 0, 1);
 
-const geometry4 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material4 = new THREE.MeshBasicMaterial({ color: 'magenta' });
-const cubePink = new THREE.Mesh(geometry4, material4);
-cubePink.position.set(-1, 1, 1);
+// const geometry4 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+// const material4 = new THREE.MeshBasicMaterial({ color: 'magenta' });
+// const cubePink = new THREE.Mesh(geometry4, material4);
+// cubePink.position.set(-1, 1, 1);
 
 // MATCAP
-const geometry5 = new THREE.TorusGeometry(0.3, 0.2, 32, 32);
+const geometry5 = new THREE.PlaneGeometry(10, 10, 64, 64);
 const material5 = new THREE.MeshStandardMaterial();
 // const material5 = new THREE.MeshMatcapMaterial();
 // material5.matcap = matcaptTexture;
@@ -155,30 +162,30 @@ const material5 = new THREE.MeshStandardMaterial();
 const cubeBlue = new THREE.Mesh(geometry5, material5);
 cubeBlue.position.set(0, 0, 0);
 
-const geometry6 = new THREE.SphereBufferGeometry(0.5, 32, 32);
-const material6 = new THREE.MeshStandardMaterial();
-const cubeWhite = new THREE.Mesh(geometry6, material6);
-cubeWhite.position.set(1, 1, 1);
-material6.metalness = 0.9;
-material6.roughness = 0.1;
-material6.envMap = envTexture;
+// const geometry6 = new THREE.SphereBufferGeometry(0.5, 32, 32);
+// const material6 = new THREE.MeshStandardMaterial();
+// const cubeWhite = new THREE.Mesh(geometry6, material6);
+// cubeWhite.position.set(1, 1, 1);
+// material6.metalness = 0.9;
+// material6.roughness = 0.1;
+// material6.envMap = envTexture;
 // material6.map = colorTexture;
 // material6.displacementMap = displacementTexture;
 // material6.bumpMap = bumpTexture;
 
 //Triangle
-const triangleGeometry = new THREE.PlaneBufferGeometry(1, 1);
+// const triangleGeometry = new THREE.PlaneBufferGeometry(1, 1);
 // const verticesArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 // const positionsAttribute = new THREE.BufferAttribute(verticesArray, 3);
 // geometry1.setAttribute('position', positionsAttribute);
 
 //SPHERE
-const sphereGeo = new THREE.SphereGeometry(0.5, 32, 16);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-  map: colorTexture,
-});
-const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
-sphere.position.set(0, -1, 1);
+// const sphereGeo = new THREE.SphereGeometry(0.5, 32, 16);
+// const sphereMaterial = new THREE.MeshBasicMaterial({
+//   map: colorTexture,
+// });
+// const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
+// sphere.position.set(0, -1, 1);
 // const wireframe = new THREE.WireFrameGeometry(sphereGeo);
 // scene.add(cube, sphere);
 
@@ -212,7 +219,7 @@ const aspect = {
 
 // const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.z = 2;
+camera.position.z = 9;
 scene.add(camera);
 
 // Renderer
@@ -248,10 +255,10 @@ function animate() {
   const elapsedTime = clock.getElapsedTime();
   orbitControls.update();
   //LookAt
-  cubeGreen.lookAt(new THREE.Vector3(-cursor.x, cursor.y));
+  // cubeGreen.lookAt(new THREE.Vector3(-cursor.x, cursor.y));
 
-  sphere.rotation.y = elapsedTime * 0.125;
-  sphere.rotation.x = elapsedTime * 0.125;
+  // sphere.rotation.y = elapsedTime * 0.125;
+  // sphere.rotation.x = elapsedTime * 0.125;
   // sphere.position.x = Math.sin(elapsedTime * 0.25);
   // sphere.position.y = Math.cos(elapsedTime * 0.25);
 
