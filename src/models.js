283730d284612cb95c6bed2ca87304b6adf6a792
Nicholas <infinitevolume@gmail.com>
2023-08-11 10:26:27 -0700

    added model module and Tpose and draco model

---
+++ src/models.js
@@ -0,0 +1,61 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
+
+//Scene
+const scene = new THREE.Scene();
+
+//Debugging
+// const gui = new dat.GUI();
+
+//Resizing
+window.addEventListener('resize', () => {
+  //Update Size
+  aspect.width = window.innerWidth;
+  aspect.height = window.innerHeight;
+
+  //New Aspect Ratio
+  camera.aspect = aspect.width / aspect.height;
+  camera.updateProjectionMatrix();
+
+  //New RendererSize
+  renderer.setSize(aspect.width, aspect.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector('.draw');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(aspect.width, aspect.height);
+
+//OrbitControls
+const orbitControls = new OrbitControls(camera, canvas);
+orbitControls.enableDamping = true;
+
+//Clock Class
+const clock = new THREE.Clock();
+
+const animate = () => {
+  //GetElapsedTime
+  const elapsedTime = clock.getElapsedTime();
+
+  //Update Controls
+  orbitControls.update();
+
+  //Renderer
+  renderer.render(scene, camera);
+
+  //RequestAnimationFrame
+  window.requestAnimationFrame(animate);
+};
+animate();
