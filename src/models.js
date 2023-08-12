d29da051a62d6c11cea8404f7c71ee0cc7bff4e5
Nicholas <infinitevolume@gmail.com>
2023-08-12 13:25:32 -0700

    added goku model to scene with animations

--- src/models.js
+++ src/models.js
@@ -11,9 +11,9 @@ import * as dat from 'dat.gui';
 const scene = new THREE.Scene();
 
 // Lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 3);
-const directionalLight = new THREE.DirectionalLight(0xbbccff, 1.8);
-directionalLight.position.z = 3;
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.8);
+directionalLight.position.z = 2;
 scene.add(ambientLight, directionalLight);
 
 //Debugging
@@ -41,19 +41,18 @@ gltfLoader.setDRACOLoader(dracoloader);
 // FBXLoader
 const fbxloader = new FBXLoader();
 
-let animationMixer = null;
-
-fbxloader.load('models/Run.fbx', (fbx) => {
-  animationMixer = new THREE.AnimationMixer(fbx);
-  const clipAction = animationMixer.clipAction(fbx.animations[0]);
-  clipAction.play();
-  fbx.scale.set(0.01, 0.01, 0.01);
-  scene.add(fbx);
-  console.log(fbx);
-});
+// let animationMixer = null;
+// fbxloader.load('models/Run.fbx', (fbx) => {
+//   animationMixer = new THREE.AnimationMixer(fbx);
+//   const clipAction = animationMixer.clipAction(fbx.animations[0]);
+//   clipAction.play();
+//   fbx.scale.set(0.01, 0.01, 0.01);
+//   scene.add(fbx);
+//   console.log(fbx);
+// });
 
 // Loading GLTF Model
-// let animationMixer = null;
+let animationMixer = null;
 
 // gltfLoader.load('models/animatedCube.glb', (glb) => {
 //   animationMixer = new THREE.AnimationMixer(glb.scene);
@@ -63,6 +62,17 @@ fbxloader.load('models/Run.fbx', (fbx) => {
 //   scene.add(glb.scene);
 //   console.log(glb);
 // });
+
+gltfLoader.load('models/newModelglb.glb', (glb) => {
+  animationMixer = new THREE.AnimationMixer(glb.scene);
+  scene.add(glb.scene);
+  const clipAction = animationMixer.clipAction(glb.animations[3]);
+  glb.scene.position.x = 2.5;
+  glb.scene.position.y = -0.8;
+  // clipAction.play();
+  console.log(glb);
+});
+
 //Resizing
 window.addEventListener('resize', () => {
   //Update Size
@@ -83,13 +93,18 @@ const aspect = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+const camera = new THREE.PerspectiveCamera(45, aspect.width / aspect.height);
 camera.position.z = 3;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector('.draw');
 const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.physicallyCorrectLights = true;
+//! renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor('#808080', 0.5);
+renderer.setSize(aspect.width, aspect.height);
 renderer.setSize(aspect.width, aspect.height);
 
 //OrbitControls
@@ -121,9 +136,3 @@ const animate = () => {
   window.requestAnimationFrame(animate);
 };
 animate();
-
-// 1) Weight
-// 2) Data Type
-// 3) Compression
-// 4) Copyright
-// 5) etc...
