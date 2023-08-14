81b81c6ac1f04652c933650da7ed103cccf9798a
Nicholas <infinitevolume@gmail.com>
2023-08-14 15:22:50 -0700

    added vertex and fragment glsl source code and applied shader

---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,18 @@
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+// uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+
+attribute vec3 position;
+
+void main() {
+    vec3 copyPosition = position;
+    copyPosition.y += copyPosition.y;
+    vec4 modelPosition = modelMatrix * vec4(copyPosition * 2.0, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition;
+}
+
