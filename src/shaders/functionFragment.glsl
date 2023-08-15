fc64ac649408d85e142853a15690540f86df5712
Nicholas <infinitevolume@gmail.com>
2023-08-15 13:23:36 -0700

    added Mix function shader to cube

---
+++ src/shaders/functionFragment.glsl
@@ -0,0 +1,23 @@
+precision mediump float;
+
+varying vec2 v_uv;
+
+void main() {
+    // vec2 copy_uv = v_uv;
+    vec3 mixing = mix(vec3(0.0,0.0,1.0), vec3(0.0,1.0,0.0), v_uv.x);
+
+
+    // if(copy_uv.x > 0.5) {
+    //     copy_uv.x = 0.0;
+    // } else {
+    //     copy_uv.x = 1.0;
+    // }
+
+    float copy_uvx = step(v_uv.x, 0.5);
+
+    // gl_FragColor = vec4(copy_uv, 1.0, 1.0);
+    // gl_FragColor = vec4(copy_uvx,v_uv.y, 1.0, 1.0);
+
+    // Mixing colors
+    gl_FragColor = vec4(mixing, 1.0);
+}
