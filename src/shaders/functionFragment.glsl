1953ca43ec6475365ac30e4115bb9ade289d883a
Nicholas <infinitevolume@gmail.com>
2023-08-21 12:21:08 -0700

    added smoothstep & modulus functions, added random noise

--- src/shaders/functionFragment.glsl
+++ src/shaders/functionFragment.glsl
@@ -3,6 +3,17 @@ precision mediump float;
 varying vec2 v_uv;
 varying vec3 v_position;
 
+
+// *Random
+float random (vec2 st) {
+    const float a = 12.9898;
+    const float b = 78.233;
+    const float c = 43758.543123;
+    return fract(sin(dot(st, vec2(a, b))) * c);
+}
+
+
+
 void main() {
     // vec2 copy_uv = v_uv;
 
@@ -20,12 +31,32 @@ void main() {
     // color.r = step(0.0, v_position.x);
     // color.g = step(0.0, v_position.y);
 
-    // *SmoothStep
-    vec3 color = vec3(1.0,1.0,1.0);
-    color.r = smoothstep(0.0, 0.04, v_position.x);
-    // color.g = smoothstep(0.0,0.04, v_position.y);
-    color.g = smoothstep(0.1,0.05, v_position.y);
+    // *SmoothStep Function
+    // vec3 color = vec3(1.0,1.0,1.0);
+    // color.r = smoothstep(0.0, 0.04, v_position.x);
+    // // color.g = smoothstep(0.0,0.04, v_position.y);
+    // color.g = smoothstep(0.1,0.05, v_position.y);
 
+
+    // *Modulus Function
+    // vec3 color = vec3(1.0, 1.0, 1.0);
+    // float mod_ =mod(v_uv.y*5.0,1.0);
+
+    // gl_FragColor = vec4(mod_,mod_,mod_,1.0);
+    
+    // *Length Function
+    // gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+    // float vector_length = length(vec2(0.4,0.3));
+    // gl_FragColor.r = vector_length;
+
+
+    // *Distance Function
+    // float distance_ = distance(v_uv, vec2(0.5));
+    // gl_FragColor = vec4(distance_,distance_,distance_,1.0);
+
+    // *RANDOM FUNCTION
+    vec3 color = random(v_uv)*vec3(.5);
+    gl_FragColor = vec4(color, 1.0);
 
 
     // if(copy_uv.x > 0.5) {
@@ -43,6 +74,6 @@ void main() {
     // gl_FragColor = vec4(mixing, 1.0);
 
     // *Clamping colors
-    gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(color, 1.0);
 
 }
