395b20ef66a94449d3ab53c2e0eab34aa1f6076c
Nicholas <infinitevolume@gmail.com>
2023-08-22 13:53:46 -0700

    added click event listener to change models with shaders

--- src/project.js
+++ src/project.js
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler';
+import { gsap } from 'gsap';
 import vShader from './shaders/vert.glsl';
 import fShader from './shaders/frag.glsl';
 
@@ -60,13 +62,35 @@ const firstModelColor1 = 'red';
 const firstModelColor2 = 'yellow';
 const secondModelColor1 = 'blue';
 const secondModelColor2 = 'white';
+const modelArray = [];
 
 // Loading Models
 // 1)
 gltfLoader.load('models/1/1.glb', (glb) => {
+  //increasing the number of vertices
+  const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
+  const particlesNumber = 25000;
+  const particlesGeometry = new THREE.BufferGeometry();
+  const particlesArray = new Float32Array(particlesNumber * 3);
+
+  for (let i = 0; i < particlesNumber; i++) {
+    const particlePosition = new THREE.Vector3();
+    samplerMesh.sample(particlePosition);
+    particlesArray.set(
+      [particlePosition.x, particlePosition.y, particlePosition.z],
+      i * 3
+    );
+  }
+
+  particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(particlesArray, 3)
+  );
+
   // changing model into particles
   glb.scene.children[0] = new THREE.Points(
-    glb.scene.children[0].geometry,
+    // glb.scene.children[0].geometry,
+    particlesGeometry,
     // new THREE.PointsMaterial({ size: 0.02 })
 
     // * if you want to use the points class with raw shader material you have
@@ -77,6 +101,7 @@ gltfLoader.load('models/1/1.glb', (glb) => {
       uniforms: {
         u_color_1: { value: new THREE.Color(`${firstModelColor1}`) },
         u_color_2: { value: new THREE.Color(`${firstModelColor2}`) },
+        u_scale: { value: 0 },
       },
       depthTest: false,
       blending: THREE.AdditiveBlending,
@@ -86,36 +111,95 @@ gltfLoader.load('models/1/1.glb', (glb) => {
   glb.scene.children[0].scale.set(0.7, 0.7, 0.7);
   glb.scene.children[0].position.x = 0.5;
   glb.scene.children[0].rotation.y = Math.PI * 0.5;
-  scene.add(glb.scene);
+  // scene.add(glb.scene);
+  modelArray[0] = glb.scene;
 });
 
 // 2)
-// gltfLoader.load('models/1/2.glb', (glb) => {
-//   // changing model into particles
-//   glb.scene.children[0] = new THREE.Points(
-//     glb.scene.children[0].geometry,
-//     // new THREE.PointsMaterial({ size: 0.02 })
+gltfLoader.load('models/1/2.glb', (glb) => {
+  //increasing the number of vertices
+  const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
+  const particlesNumber = 25000;
+  const particlesGeometry = new THREE.BufferGeometry();
+  const particlesArray = new Float32Array(particlesNumber * 3);
 
-//     // * if you want to use the points class with raw shader material you have
-//     // * to use gl_PointSize in vertex.glsl file
-//     new THREE.RawShaderMaterial({
-//       vertexShader: vShader,
-//       fragmentShader: fShader,
-//       uniforms: {
-//         u_color_1: { value: new THREE.Color(`${secondModelColor1}`) },
-//         u_color_2: { value: new THREE.Color(`${secondModelColor2}`) },
-//       },
-//       depthTest: false,
-//       blending: THREE.AdditiveBlending,
-//     })
-//   );
+  for (let i = 0; i < particlesNumber; i++) {
+    const particlePosition = new THREE.Vector3();
+    samplerMesh.sample(particlePosition);
+    particlesArray.set(
+      [particlePosition.x, particlePosition.y, particlePosition.z],
+      i * 3
+    );
+  }
 
-//   glb.scene.children[0].scale.set(0.7, 0.7, 0.7);
-//   glb.scene.children[0].position.y = -0.2;
-//   glb.scene.children[0].rotation.x = -Math.PI * 0.5;
-//   glb.scene.children[0].rotation.z = -Math.PI * 0.5;
-//   scene.add(glb.scene);
-// });
+  particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(particlesArray, 3)
+  );
+  // changing model into particles
+  glb.scene.children[0] = new THREE.Points(
+    // glb.scene.children[0].geometry,
+    particlesGeometry,
+    // new THREE.PointsMaterial({ size: 0.02 })
+
+    // * if you want to use the points class with raw shader material you have
+    // * to use gl_PointSize in vertex.glsl file
+    new THREE.RawShaderMaterial({
+      vertexShader: vShader,
+      fragmentShader: fShader,
+      uniforms: {
+        u_color_1: { value: new THREE.Color(`${secondModelColor1}`) },
+        u_color_2: { value: new THREE.Color(`${secondModelColor2}`) },
+        u_scale: { value: 0 },
+      },
+      depthTest: false,
+      blending: THREE.AdditiveBlending,
+    })
+  );
+
+  glb.scene.children[0].scale.set(0.3, 0.3, 0.3);
+  glb.scene.children[0].position.y = -0.2;
+  glb.scene.children[0].rotation.x = -Math.PI * 0.5;
+  glb.scene.children[0].rotation.z = -Math.PI * 0.5;
+  // scene.add(glb.scene);
+  modelArray[1] = glb.scene;
+});
+
+// *BUTTON LISTENERS
+//first button
+buttons[0].addEventListener('click', () => {
+  gsap.to(modelArray[0].children[0].material.uniforms.u_scale, {
+    value: 1,
+    duration: 1,
+  });
+
+  gsap.to(modelArray[1].children[0].material.uniforms.u_scale, {
+    value: 0,
+    duration: 1,
+    onComplete: () => {
+      scene.remove(modelArray[1]);
+    },
+  });
+
+  scene.add(modelArray[0]);
+});
+
+// second button
+buttons[1].addEventListener('click', () => {
+  gsap.to(modelArray[1].children[0].material.uniforms.u_scale, {
+    value: 1,
+    duration: 1,
+  });
+
+  gsap.to(modelArray[0].children[0].material.uniforms.u_scale, {
+    value: 0,
+    duration: 1,
+    onComplete: () => {
+      scene.remove(modelArray[0]);
+    },
+  });
+  scene.add(modelArray[1]);
+});
 
 //OrbitControl
 const orbitControls = new OrbitControls(camera, canvas);
